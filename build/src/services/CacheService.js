eaea24a9c8a95670a6d60dbf60c682d491f9293e
isfo <isfo@live.com>
2022-09-25 21:25:29 -0300

    ajustes

--- build/src/services/CacheService.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.myCache = void 0;
-const NodeCache = require("node-cache");
-const myCache = new NodeCache();
-exports.myCache = myCache;
-//# sourceMappingURL=CacheService.js.map
