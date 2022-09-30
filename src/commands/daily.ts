333f871c35101af656ff622d11d64c3e000ea41a
Itallo Fidelis <itallo.fidelis@wakke.co>
2022-09-30 13:47:49 -0300

    Update daily.ts

--- src/commands/daily.ts
+++ src/commands/daily.ts
@@ -12,18 +12,13 @@ const tagRole = 'dev';
 const roomName = 'daily';
 
 module.exports = async (client: Client, message: Message) => {
-
-    console.log('rodando');
     await message.guild?.members.fetch();
-
 
     const tasks = (await message.guild?.channels.fetch())?.map(async c => {
         return await c.fetch(true)        
     });
 
-
     const guildChannels: GuildChannel[] = await Promise.all(tasks ?? []);
-
 
     let dailyChannel = guildChannels?.find(channel => channel.name.toLowerCase() === roomName);
 
@@ -34,11 +29,9 @@ module.exports = async (client: Client, message: Message) => {
         });
     }
 
-
     const canais = guildChannels?.filter(x => x.type === ChannelType.GuildVoice && x.members.size > 0);
 
     let usersByChannel: { member: string, member_name: string, channel_id: string | null, channel_name: string | undefined }[] = [];
-
 
     const members = reduce(canais?.map(c => {
         return c.members.map(m => ({
